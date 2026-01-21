d77e51bb09042a5ee6992d6785b0498bee77d05d
jason0925pig-rgb <jason0925pig@gmail.com>
2026-01-21 16:58:34 +0000

    adding snowmanhead

---
+++ snowmanhead.py
@@ -0,0 +1,12 @@
+def snowmanhead():
+    print("         ___________")
+    print("        |           |")
+    print("      __|___________|__")
+    print("     /                 \\")
+    print("    |   (*)       (*)   |")
+    print("    |         |         |")
+    print("     \\       \\_/       /")
+    print("      \\_______________/")
+
+if __name__ == "__main__":
+    snowmanhead()
