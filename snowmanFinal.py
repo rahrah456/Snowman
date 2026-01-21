1c40e695b8fd2adafe4342845c158a3dd1b31231
r ranjan <rranjan@rs-MacBook-Pro.local>
2026-01-21 17:04:03 +0000

    Combining both files

---
+++ snowmanFinal.py
@@ -0,0 +1,31 @@
+def snowman_head():
+    print("         ___________")
+    print("        |           |")
+    print("      __|___________|__")
+    print("     /                 \\")
+    print("    |   (*)       (*)   |")
+    print("    |         |         |")
+    print("     \\       \\_/       /")
+    print("      \\_______________/")
+
+
+def snowman_base():
+    print("- -- - - - -- - -- - - - - --")
+    print("- -- - - - -- - -- - - - - --")
+
+def snowman_body():
+    print("        . -- ~~~ -- .")
+    print("    .-~               ~-.")
+    print("   /                     \\")
+    print("  /                       \\")
+    print(" |                         |")
+    print(" |                         |")
+    print(" |                         |")
+    print("  \                       /")
+    print("   \                     /")
+    print("    `-.               .-'")
+    print("        ~- . ___ . -~")
+
+snowman_head()
+snowman_body()
+snowman_base()
