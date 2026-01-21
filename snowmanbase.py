09dd113e1f84053abc02e0ab37af70a631a7b598
r ranjan <rranjan@rs-MacBook-Pro.local>
2026-01-21 16:45:15 +0000

    adding snowman base and body

---
+++ snowmanbase.py
@@ -0,0 +1,19 @@
+def snowman_base():
+    print("- -- - - - -- - -- - - - - --")
+    print("- -- - - - -- - -- - - - - --")
+
+def snowman_body():
+    print("        . -- ~~~ -- .")
+    print("    .-~               ~-.")
+    print("   /                     \\")
+    print("  /                       \\")
+    print(" |                         |")
+    print(" |                         |")
+    print(" |                         |")
+    print("  \                       /")
+    print("   \                     /")
+    print("    `-.               .-'")
+    print("        ~- . ___ . -~")
+
+snowman_body()
+snowman_base()
